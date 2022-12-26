e43be1fbf30eac3750038ffa307fe6cf584d0a54
Janaka Low <chee-kong.low@vitrox.com>
2022-12-26 17:08:02 +0800

    Added more comments in Config.py file.

--- Config.py
+++ Config.py
@@ -1,13 +1,20 @@
 """
 Please edit this file to configure the system
 """
-CAMERA_ID = 0  # 0: First camera in the system; 1: Second camera, 2: Third camera, etc.
+
+# 0: First camera in the system; 1: Second camera, 2: Third camera, etc.
+CAMERA_ID = 0
+
+# EXE file with full path. Note that forward slash (/) must be used instead of backslash (\)
 VLC_EXE = "C:/Program Files/VideoLAN/VLC/vlc.exe"
+
+# specify the folder in which JSY video files are located. Video files must be .mp4 files.
+# Note that forward slash (/) must be used instead of backslash (\)
 JSY_VIDOE_PATH = "C:/Users/chee-kong.low/PycharmProjects/Bowing/jsy_videos"
 
 # Set to True for production mode to show minimum information. Se to False to show detailed information.
 IS_PRODUCTION = False
 
-# Camera resoltions
+# Camera resolutions
 CAM_WIDTH = 640
 CAM_HEIGHT = 480
